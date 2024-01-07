c75b827af08e0749fccd03c433dee0d034e4bd45
Eudes Lima <eudesfilho1994@outlook.com>
2024-01-07 10:08:34 -0300

    solutions of page 20

--- solutions/chapter_two/name.py
+++ solutions/chapter_two/name.py
@@ -8,3 +8,20 @@ last_name = "Lovelace"
 
 full_name = f"{first_name} {last_name}"
 print("Full name: "+full_name)
+
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = ' python '
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+favorite_language = favorite_language.lstrip()
+print(favorite_language)
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+nostarch_url = 'https://nostarch.com'
+simple_url = nostarch_url.removeprefix('https://')
+print(simple_url)
