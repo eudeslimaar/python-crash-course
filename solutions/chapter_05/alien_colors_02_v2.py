33b8f624928cc4253a0822694c4d120fad42fb32
Eudes Lima <eudesfilho1994@outlook.com>
2024-01-30 20:18:12 -0300

    Added solutions to exercises in Chapter 5

---
+++ solutions/chapter_05/alien_colors_02_v2.py
@@ -0,0 +1,8 @@
+alien_color = 'yellow'
+
+if alien_color == 'green':
+    print('You earned 5 points')
+if alien_color == 'yellow':
+    print('You earned 10 points')
+else:
+    print()
--- solutions/chapter_05/toppings.py
+++ solutions/chapter_05/toppings.py
@@ -1,4 +1,11 @@
-requested_toppings = 'mushrooms'
+requested_toppings = ['mushrooms', 'extra cheese']
 
 if requested_toppings != 'anchovies':
     print("Hold the anchovies!")
+
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+
+print("\nFinished making pizza")
