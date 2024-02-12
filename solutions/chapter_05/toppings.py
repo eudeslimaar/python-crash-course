e79cf22dda10248aaec3e4bdb649ff18ac073fc6
Eudes Lima <eudesfilho1994@outlook.com>
2024-02-12 08:44:31 -0300

    Update toppings

--- solutions/chapter_05/toppings.py
+++ solutions/chapter_05/toppings.py
@@ -1,11 +1,15 @@
-requested_toppings = ['mushrooms', 'extra cheese']
+available_toppings = ['mushrooms', 'olives', 'green peppers',
+                      'pepperoni', 'pineapple', 'extra cheese']
 
-if requested_toppings != 'anchovies':
-    print("Hold the anchovies!")
+requested_toppings = ['mushrooms', 'french fries', 'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-    print("Adding mushrooms.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        if requested_topping in available_toppings:
+            print(f'Adding {requested_topping}.')
+        else:
+            print(f"Sorry, we don't have {requested_topping}.")
 
-print("\nFinished making pizza")
+    print("\nFinished making your pizza")
+else:
+    print("Are you sure you want a plain pizza?")
