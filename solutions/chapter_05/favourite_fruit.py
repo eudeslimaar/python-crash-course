1d9ec91fc13c9b48465090a6d7781480655811e5
Eudes Lima <eudesfilho1994@outlook.com>
2024-02-12 08:38:54 -0300

    Added solutions to exercises in Chapter 5

---
+++ solutions/chapter_05/favourite_fruit.py
@@ -0,0 +1,13 @@
+favourite_fruits = ['passion fruit', 'mango', 'watermelon', 'lemon', 'coco']
+
+for fruit in favourite_fruits:
+    if fruit == 'passion fruit':
+        print('You really like passion fruit!')
+    if fruit == 'mango':
+        print('You really like mango!')
+    if fruit == 'watermelon':
+        print('You really like watermelon!')
+    if fruit == 'lemon':
+        print('You really like lemon!')
+    if fruit == 'coco':
+        print('You really like coco!')
---
+++ solutions/chapter_05/stages_of_life.py
@@ -0,0 +1,14 @@
+person = 13
+
+if person < 2:
+    print('The person is a baby')
+elif 2 <= person < 4:
+    print('The person is a toddler')
+elif 4 <= person < 13:
+    print('The person is a kid')
+elif 13 <= person < 20:
+    print('The person is a teenager')
+elif 20 <= person < 65:
+    print('the person is an adult')
+else:
+    print('The person is a elder')
